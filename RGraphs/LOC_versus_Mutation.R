56eb4d613884beacf3441fcff181422f55e63f6e
jakepetrovic <jake.z.petrovic@gmail.com>
2014-06-07 14:10:44 -0600

    Specific LOC bar graph added

--- RGraphs/LOC_versus_Mutation.R
+++ RGraphs/LOC_versus_Mutation.R
@@ -4,7 +4,7 @@ DoubleChecksoniTrust <- read.csv("D:/Documents/GitHub/Paper/Jesh_Jacob_QualityOf
 data <- DoubleChecksoniTrust
 
 # CodePro ---------------------------------------------------------
-p1 <- ggplot(data, aes(x=CPLOC, y=CPMutation))
+p1 <- ggplot(data, aes(x=SLOC, y=CPMutation))
 p2 <- p1 + theme_bw()
 p3 <- p2 + labs(title="Mutation Score versus LOC- CodePro", x = "LOC", y="Mutation Score (%)")
 
@@ -12,7 +12,7 @@ p4_line <- p3 + geom_smooth(method="lm",formula = y~ x)
 p4_poly <- p3 + geom_smooth(method="lm",formula = y~ poly(x,2))
 
 lm_poly_eqn = function(df){
-m=lm(CPMutation ~ poly(CPLOC, 2), df)#2nd degree polynomial
+m=lm(CPMutation ~ poly(SLOC, 2), df)#2nd degree polynomial
 eq <- substitute(italic(y) == a + b %.% italic(x)* + c %.% italic(x)^2* ","~~italic(r)^2~"="~r2,
 list(a = format(coef(m)[1], digits = 2),
 b = format(coef(m)[2], digits = 2),
@@ -22,7 +22,7 @@ as.character(as.expression(eq))
 }
 
 lm_line_eqn = function(df){
-m=lm(CPMutation ~ CPLOC, df)#2nd degree polynomial-- need to fix
+m=lm(CPMutation ~ SLOC, df)#2nd degree polynomial-- need to fix
 eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
 list(a = format(coef(m)[1], digits = 2),
 b = format(coef(m)[2], digits = 2),
@@ -40,7 +40,7 @@ p5_poly + geom_point()
 ggsave("D:/Documents/GitHub/Paper/Jesh_Jacob_QualityOfTestsPaper/RGraphs/CodePro_LOC_versus_Mutation_poly.pdf")
 
 # Manual ---------------------------------------------------------
-p1 <- ggplot(data, aes(x=MLOC, y=MMutation))
+p1 <- ggplot(data, aes(x=SLOC, y=MMutation))
 p2 <- p1 + theme_bw()
 p3 <- p2 + labs(title="Mutation Score versus LOC- Manual", x = "LOC", y="Mutation Score (%)")
 
@@ -48,7 +48,7 @@ p4_line <- p3 + geom_smooth(method="lm",formula = y~ x)
 p4_poly <- p3 + geom_smooth(method="lm",formula = y~ poly(x,2))
 
 lm_poly_eqn = function(df){
-m=lm(MMutation ~ poly(MLOC, 2), df)#2nd degree polynomial
+m=lm(MMutation ~ poly(SLOC, 2), df)#2nd degree polynomial
 eq <- substitute(italic(y) == a + b %.% italic(x)* + c %.% italic(x)^2* ","~~italic(r)^2~"="~r2,
 list(a = format(coef(m)[1], digits = 2),
 b = format(coef(m)[2], digits = 2),
@@ -58,7 +58,7 @@ as.character(as.expression(eq))
 }
 
 lm_line_eqn = function(df){
-m=lm(MMutation ~ MLOC, df)#2nd degree polynomial-- need to fix
+m=lm(MMutation ~ SLOC, df)#2nd degree polynomial-- need to fix
 eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
 list(a = format(coef(m)[1], digits = 2),
 b = format(coef(m)[2], digits = 2),
@@ -77,7 +77,7 @@ ggsave("D:/Documents/GitHub/Paper/Jesh_Jacob_QualityOfTestsPaper/RGraphs/Manual_
 
 
 # Evosuite ---------------------------------------------------------
-p1 <- ggplot(data, aes(x=ELOC, y=EMutation))
+p1 <- ggplot(data, aes(x=SLOC, y=EMutation))
 p2 <- p1 + theme_bw()
 p3 <- p2 + labs(title="Mutation Score versus LOC- Evosuite", x = "LOC", y="Mutation Score (%)")
 
@@ -85,7 +85,7 @@ p4_line <- p3 + geom_smooth(method="lm",formula = y~ x)
 p4_poly <- p3 + geom_smooth(method="lm",formula = y~ poly(x,2))
 
 lm_poly_eqn = function(df){
-m=lm(EMutation ~ poly(ELOC, 2), df)#2nd degree polynomial
+m=lm(EMutation ~ poly(SLOC, 2), df)#2nd degree polynomial
 eq <- substitute(italic(y) == a + b %.% italic(x)* + c %.% italic(x)^2* ","~~italic(r)^2~"="~r2,
 list(a = format(coef(m)[1], digits = 2),
 b = format(coef(m)[2], digits = 2),
@@ -95,7 +95,7 @@ as.character(as.expression(eq))
 }
 
 lm_line_eqn = function(df){
-m=lm(EMutation ~ ELOC, df)#2nd degree polynomial-- need to fix
+m=lm(EMutation ~ SLOC, df)#2nd degree polynomial-- need to fix
 eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
 list(a = format(coef(m)[1], digits = 2),
 b = format(coef(m)[2], digits = 2),
