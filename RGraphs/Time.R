131d2279b85e53f432b865c7791d503ea63986d3
Kristen Walcott-Justice <kjustice@uccs.edu>
2014-06-06 18:59:05 -0600

    Timing Graph

--- RGraphs/Time.R
+++ RGraphs/Time.R
@@ -4,10 +4,11 @@ DoubleChecksoniTrust <- read.csv("/Users/kjustice/Documents/UCCS Classes/papers/
 data <- DoubleChecksoniTrust
 
 dm.molten <- melt (data, na.rm= TRUE, variable.name="tested")
-ggplot(data=dm.molten, aes(x=variable, y=value, fill=Program)) + geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+labs(title="Time of Test Case Generation", x = "Programs", y="Time to Generate (s)")+guides(fill=guide_legend(title="Generation Tools"))+ 
-    geom_bar(position=position_dodge(), stat="identity") +
-    geom_errorbar(aes(ymin=len-se, ymax=len+se),
-                  width=.2,                    # Width of the error bars
-                  position=position_dodge(.9))
+ggplot(data=dm.molten, aes(x=variable, y=value, fill=Program)) + geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+labs(title="Time of Test Case Generation", x = "Programs", y="Time to Generate (s)")
+#+guides(fill=guide_legend(title="Generation Tools"))+ 
+#    geom_bar(position=position_dodge(), stat="identity") +
+#    geom_errorbar(aes(ymin=len-se, ymax=len+se),
+#                  width=.2,                    # Width of the error bars
+#                  position=position_dodge(.9))
 
 ggsave("/Users/kjustice/Documents/UCCS Classes/papers/Jesh_Jacob_QualityOfTestsPaper/RGraphs/TestCasesGenerated.pdf")
