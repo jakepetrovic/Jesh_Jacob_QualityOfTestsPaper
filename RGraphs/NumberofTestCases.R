691f2215318239b2ed4bceda9d9dc6b043d884c0
Kristen Walcott-Justice <kjustice@uccs.edu>
2014-06-07 21:19:38 -0600

    Fixed Number of Test Cases generated for graph based on Jesh's eval of manual data

--- RGraphs/NumberofTestCases.R
+++ RGraphs/NumberofTestCases.R
@@ -1,10 +1,11 @@
 library("ggplot2")
 library(reshape2)
 DoubleChecksoniTrust <- read.csv("/Users/kjustice/Documents/UCCS Classes/papers/Jesh_Jacob_QualityOfTestsPaper/RGraphs/TestCasesGenerated.csv")
-data <- DoubleChecksoniTrust
 
-dm.molten <- melt (data, na.rm= TRUE, variable.name="tested")
-p = (ggplot(data=dm.molten, aes(x=variable, y=value, fill=Program)) + geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+labs(title="Number of JUnit Test Cases", x = "Programs", y="Number of Test Cases"))
+mydata <- DoubleChecksoniTrust
+limits <- aes(ymax = mydata$upper, ymin = mydata$lower)
+p = (ggplot(data = mydata, aes(x = variable, fill = Program, y = value)) + geom_bar(position = "dodge", 
+    stat = "identity") + geom_errorbar(limits, width = 0.9, position = "dodge")+ theme_minimal()+labs(title="Number of JUnit Test Cases", x = "Programs", y="Number of Test Cases"))
 p = p + guides(fill=guide_legend(title="Generation Tools"))
 (p = p + scale_fill_grey(start = 0, end = .9))
 p = p +theme_bw()
