1b43dd23df5b401e83df872f317d134d3402a09b
Kristen Walcott-Justice <kjustice@uccs.edu>
2014-06-07 18:46:45 -0600

    Fixing some graphs and adding 4 key graph sets into eval (missing time to execute)

--- RGraphs/NumberofTestCases.R
+++ RGraphs/NumberofTestCases.R
@@ -4,7 +4,7 @@ DoubleChecksoniTrust <- read.csv("/Users/kjustice/Documents/UCCS Classes/papers/
 data <- DoubleChecksoniTrust
 
 dm.molten <- melt (data, na.rm= TRUE, variable.name="tested")
-p = (ggplot(data=dm.molten, aes(x=variable, y=value, fill=program)) + geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+labs(title="Number of JUnit Test Cases", x = "Programs", y="Number of Test Cases"))
+p = (ggplot(data=dm.molten, aes(x=variable, y=value, fill=Program)) + geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+labs(title="Number of JUnit Test Cases", x = "Programs", y="Number of Test Cases"))
 p = p + guides(fill=guide_legend(title="Generation Tools"))
 (p = p + scale_fill_grey(start = 0, end = .9))
 p = p +theme_bw()
