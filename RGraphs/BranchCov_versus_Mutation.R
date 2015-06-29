5e5bcf87d74114389823c09e9feecbf4580d6c43
Jesh <jskracht@gmail.com>
2015-06-28 18:13:08 -0600

    Updated Graph Data

--- RGraphs/BranchCov_versus_Mutation.R
+++ RGraphs/BranchCov_versus_Mutation.R
@@ -110,3 +110,39 @@ p5_line + geom_point()
 ggsave("Evosuite_BranchCov_versus_Mutation_line.pdf")
 p5_poly + geom_point()
 ggsave("Evosuite_BranchCov_versus_Mutation_poly.pdf")
+
+# Randoop ---------------------------------------------------------
+p1 <- ggplot(data, aes(x=RBranchCov, y=RMutation))
+p2 <- p1 + theme_bw()
+p3 <- p2 + labs(title="Mutation Score versus Branch Coverage- Randoop", x = "Branch Coverage (%)", y="Mutation Score (%)")
+
+p4_line <- p3 + geom_smooth(method="lm",formula = y~ x)
+p4_poly <- p3 + geom_smooth(method="lm",formula = y~ poly(x,2))
+
+lm_poly_eqn = function(df){
+m=lm(RMutation ~ poly(RBranchCov, 2), df)#2nd degree polynomial
+eq <- substitute(italic(y) == a + b %.% italic(x)* + c %.% italic(x)^2* ","~~italic(r)^2~"="~r2,
+list(a = format(coef(m)[1], digits = 2),
+b = format(coef(m)[2], digits = 2),
+c = format(coef(m)[3], digits = 2),
+r2 = format(summary(m)$r.squared, digits = 3)))
+as.character(as.expression(eq))
+}
+
+lm_line_eqn = function(df){
+m=lm(CPMutation ~ CPBranchCov, df)#2nd degree polynomial-- need to fix
+eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
+list(a = format(coef(m)[1], digits = 2),
+b = format(coef(m)[2], digits = 2),
+c = format(coef(m)[3], digits = 2),
+r2 = format(summary(m)$r.squared, digits = 3)))
+as.character(as.expression(eq))
+}
+
+p5_line <- p4_line + annotate("text", x=.5, y=45, label=lm_line_eqn(data), hjust=0, size=6, family="Times", face="italic", parse=TRUE)
+p5_poly <- p4_poly + annotate("text", x=.5, y=45, label=lm_poly_eqn(data), hjust=0, size=6, family="Times", face="italic", parse=TRUE)
+
+p5_line + geom_point()
+ggsave("Randoop_BranchCov_versus_Mutation_line.pdf")
+p5_poly + geom_point()
+ggsave("Randoop_BranchCov_versus_Mutation_poly.pdf")
