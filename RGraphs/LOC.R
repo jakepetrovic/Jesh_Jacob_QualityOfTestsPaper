490a76107a577fef2bbafa5440a0f2f2287ab7e3
Jesh <jskracht@gmail.com>
2015-06-28 19:39:54 -0600

    Added Random NCSS Data, Update more Graphs

--- RGraphs/LOC.R
+++ RGraphs/LOC.R
@@ -1,7 +1,6 @@
 library("ggplot2")
 library(reshape2)
-#DoubleChecksoniTrust <- read.csv("D:/Documents/GitHub/Paper/Jesh_Jacob_QualityOfTestsPaper/RGraphs/LOC.csv")
-DoubleChecksoniTrust <- read.csv("/Users/kjustice/Documents/UCCS Classes/papers/Jesh_Jacob_QualityOfTestsPaper/RGraphs/LOC.csv")
+DoubleChecksoniTrust <- read.csv("LOC.csv")
 mydata <- DoubleChecksoniTrust
 
 limits <- aes(ymax = mydata$upper, ymin = mydata$lower)
@@ -10,5 +9,4 @@ p = (ggplot(data = mydata, aes(x = variable, fill = Program, y = value)) + geom_
 p = p + guides(fill=guide_legend(title="Code Considered"))
 (p = p + scale_fill_grey(start = 0, end = .9))
 
-ggsave("/Users/kjustice/Documents/UCCS Classes/papers/Jesh_Jacob_QualityOfTestsPaper/RGraphs/LOC.pdf", height = 7, width = 11)
-#ggsave("D:/Documents/GitHub/Paper/Jesh_Jacob_QualityOfTestsPaper/RGraphs/LOC.pdf", height = 7, width = 17)
+ggsave("LOC.pdf", height = 7, width = 17)
